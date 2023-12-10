7009d517ff4c2ceb12bb704a9bbf5bb1401592f2
Ashwina Kalanathan <124553081+AK23CS@users.noreply.github.com>
2023-12-10 16:49:03 +0000

    Update seed.py

--- Pelican/tasks/management/commands/seed.py
+++ Pelican/tasks/management/commands/seed.py
@@ -2,36 +2,36 @@ from django.core.management.base import BaseCommand
 from tasks.models import User
 from faker import Faker
 from random import randint
-
+ 
 user_fixtures = [
     {'username': '@johndoe', 'email': 'john.doe@example.org', 'first_name': 'John', 'last_name': 'Doe'},
     {'username': '@janedoe', 'email': 'jane.doe@example.org', 'first_name': 'Jane', 'last_name': 'Doe'},
     {'username': '@charlie', 'email': 'charlie.johnson@example.org', 'first_name': 'Charlie', 'last_name': 'Johnson'},
 ]
-
+ 
 class Command(BaseCommand):
     """Build automation command to seed the database."""
-
+ 
     USER_COUNT = 300
     DEFAULT_PASSWORD = 'Password123'
     help = 'Seeds the database with sample data'
-
+ 
     def __init__(self):
         super().__init__()
         self.faker = Faker('en_GB')
-
+ 
     def handle(self, *args, **options):
         self.create_users()
         self.users = User.objects.all()
-
+ 
     def create_users(self):
         self.generate_user_fixtures()
         self.generate_random_users()
-
+ 
     def generate_user_fixtures(self):
         for data in user_fixtures:
             self.try_create_user(data)
-
+ 
     def generate_random_users(self):
         user_count = User.objects.count()
         while user_count < self.USER_COUNT:
@@ -39,20 +39,20 @@ class Command(BaseCommand):
             self.generate_user()
             user_count = User.objects.count()
         print("User seeding complete.")
-
+ 
     def generate_user(self):
         first_name = self.faker.first_name()
         last_name = self.faker.last_name()
         email = create_email(first_name, last_name)
         username = create_username(first_name, last_name)
         self.try_create_user({'username': username, 'email': email, 'first_name': first_name, 'last_name': last_name})
-
+ 
     def try_create_user(self, data):
         try:
             self.create_user(data)
         except:
             pass
-
+ 
     def create_user(self, data):
         User.objects.create_user(
             username=data['username'],
@@ -61,9 +61,11 @@ class Command(BaseCommand):
             first_name=data['first_name'],
             last_name=data['last_name'],
         )
-
+ 
 def create_username(first_name, last_name):
     return '@' + first_name.lower() + last_name.lower()
-
+ 
 def create_email(first_name, last_name):
     return f"{first_name.lower()}.{last_name.lower()}@example.org"
+
+
