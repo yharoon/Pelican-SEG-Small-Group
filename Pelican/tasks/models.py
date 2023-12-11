327ffb2d7ef78de798357753ad0306e42b67e7f9
Ashwina Kalanathan <124553081+AK23CS@users.noreply.github.com>
2023-12-11 12:25:08 +0000

    Update models.py with invite

--- Pelican/tasks/models.py
+++ Pelican/tasks/models.py
@@ -2,6 +2,7 @@ from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from libgravatar import Gravatar
+from django.conf import settings
 
 class User(AbstractUser):
     """Model used for user authentication, and team member related information."""
@@ -52,3 +53,30 @@ class Task(models.Model):
 
     def __str__(self):
         return self.description
+
+class Invitation(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_invitations')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='received_invitations')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    accepted = models.BooleanField(default=False)
+
+    def accept(self):
+        self.receiver.teams.add(self.team)
+        self.accepted = True
+        self.save()
+
+    def decline(self):
+        self.accepted = False
+        self.save()
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    message = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    invitation = models.ForeignKey(Invitation, on_delete=models.CASCADE, related_name='related_notification', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.message}"
+
+
+
