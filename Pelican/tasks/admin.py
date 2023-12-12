69ac89ba8e5b8d80347fe18a231e84d97e7897cb
Haroon Yasin <haroon.yasin95@gmail.com>
2023-12-12 16:25:04 +0000

    actual merge / minor refactor (fixing small errors)

--- Pelican/tasks/admin.py
+++ Pelican/tasks/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from tasks.models import User,Team,Task
+from tasks.models import User,Team,Task,Invitation,Notification
 
 admin.site.register(User)
 admin.site.register(Team)
 admin.site.register(Task)
+admin.site.register(Invitation)
+admin.site.register(Notification)
--- Pelican/tasks/management/commands/seed.py
+++ Pelican/tasks/management/commands/seed.py
@@ -12,9 +12,9 @@ user_fixtures = [
 class Command(BaseCommand):
     """Build automation command to seed the database."""
 
-    USER_COUNT = 50
-    TEAM_COUNT = 50
-    TASK_COUNT = 50
+    USER_COUNT = 20
+    TEAM_COUNT = 5
+    TASK_COUNT = 5
     DEFAULT_PASSWORD = 'Password123'
     help = 'Seeds the database with sample data'
 
