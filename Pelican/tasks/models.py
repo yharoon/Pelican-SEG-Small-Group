ffd72c01aff7b882669827e46fc57310cea6e702
onyi-dozie <146564483+onyi-dozie@users.noreply.github.com>
2023-12-12 16:50:13 +0000

    Update models.py

--- Pelican/tasks/models.py
+++ Pelican/tasks/models.py
@@ -18,25 +18,37 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=50, blank=False)
     email = models.EmailField(unique=True, blank=False)
 
-
     class Meta:
         """Model options."""
-
         ordering = ['last_name', 'first_name']
 
     def full_name(self):
         """Return a string containing the user's full name."""
-
         return f'{self.first_name} {self.last_name}'
 
     def gravatar(self, size=120):
         """Return a URL to the user's gravatar."""
-
         gravatar_object = Gravatar(self.email)
         gravatar_url = gravatar_object.get_image(size=size, default='mp')
         return gravatar_url
 
     def mini_gravatar(self):
         """Return a URL to a miniature version of the user's gravatar."""
-        
         return self.gravatar(size=60)
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    members = models.ManyToManyField(User, related_name='teams', null=True)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    description = models.CharField(max_length=255)
+    due_date = models.DateField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='tasks')
+    assigned_to = models.ManyToManyField(User, related_name='assigned_tasks')
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.description
